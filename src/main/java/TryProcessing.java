992a631dbc8b82e6e894e27f4033b6fac61cf086
Akshit Batra <akbatra567@gmail.com>
2021-02-22 18:22:33 +0530

    Initial Commit

---
+++ src/main/java/TryProcessing.java
@@ -0,0 +1,14 @@
+import processing.core.PApplet;
+
+public class TryProcessing extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("TryProcessing", args);
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(640, 480);
+    }
+}
