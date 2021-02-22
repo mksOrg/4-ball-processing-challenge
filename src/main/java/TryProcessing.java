865a7aed4962ef3d367d86ef8a345e245cadd173
Akshit Batra <akbatra567@gmail.com>
2021-02-23 02:20:03 +0530

    Draw circle on mouse location

--- src/main/java/TryProcessing.java
+++ src/main/java/TryProcessing.java
@@ -2,13 +2,36 @@ import processing.core.PApplet;
 
 public class TryProcessing extends PApplet {
 
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+    public static final int DIAMETER = 10;
+
     public static void main(String[] args) {
         PApplet.main("TryProcessing", args);
     }
 
     @Override
     public void settings() {
-        super.settings();
-        size(640, 480);
+        System.out.println("Printed from the settings()");
+        size(WIDTH, HEIGHT);
+    }
+
+    @Override
+    public void setup() {
+        System.out.println("Printed from the setup()");
+    }
+
+    @Override
+    public void draw() {
+        getBackground();
+        drawCircle();
+    }
+
+    private void drawCircle() {
+        ellipse(mouseX, mouseY,DIAMETER, DIAMETER);
+    }
+
+    private void getBackground() {
+        background(255);
     }
 }
